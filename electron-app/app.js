2f461008eee5877e91fe6e11f5784ce08eef8662
Alex Reed <alexreed.atr@gmail.com>
2023-02-21 20:36:33 -0800

    making first builds

--- electron-app/app.js
+++ electron-app/app.js
@@ -1,5 +1,9 @@
 const { app, BrowserWindow, Tray } = require('electron')
 const path = require('path')
+var electron_notarize = require('electron-notarize');
+
+
+let tray = null;
 
 function createWindow() {
     const win = new BrowserWindow({
@@ -11,19 +15,27 @@ function createWindow() {
         }
     })
 
-    win.loadFile('../index.html')
-
-    const iconPath = path.join(__dirname, '../favicon.png');
-    app.dock.setIcon(iconPath);
+    win.loadFile('index.html')
 }
 
 app.whenReady().then(() => {
     createWindow()
-    app.on('activate', () => {
+
+    // Set the application icon in the dock (macOS only)
+    if (process.platform === 'darwin') {
+        const iconPath = path.join(__dirname, 'favicon.png');
+        app.dock.setIcon(iconPath);
+    }
+
+    // Set the application icon in the system tray
+    tray = new Tray(path.join(__dirname, 'favicon.png'));
+
+    tray.setToolTip('Transcript Editor');
+    tray.on('click', () => {
         if (BrowserWindow.getAllWindows().length === 0) {
             createWindow()
         }
-    })
+    });
 })
 
 app.on('window-all-closed', () => {
@@ -31,3 +43,35 @@ app.on('window-all-closed', () => {
         app.quit()
     }
 })
+
+
+module.exports = async function (params) {
+    // Only notarize the app on Mac OS only.
+    if (process.platform !== 'darwin') {
+        return;
+    }
+    console.log('afterSign hook triggered', params);
+
+    // Same appId in electron-builder.
+    let appId = '<Change this to your appId ad defined in your electron-builder config>'
+
+    let appPath = path.join(params.appOutDir, `${params.packager.appInfo.productFilename}.app`);
+    if (!fs.existsSync(appPath)) {
+        throw new Error(`Cannot find application at: ${appPath}`);
+    }
+
+    console.log(`Notarizing ${appId} found at ${appPath}`);
+
+    try {
+        await electron_notarize.notarize({
+            appBundleId: appId,
+            appPath: appPath,
+            appleId: process.env.appleId,
+            appleIdPassword: process.env.appleIdPassword,
+        });
+    } catch (error) {
+        console.error(error);
+    }
+
+    console.log(`Done notarizing ${appId}`);
+};
---
+++ electron-app/script.js
@@ -0,0 +1,89 @@
+const editor = document.getElementById("editor"),
+    transcript = document.getElementById("transcript")
+
+function addPrefix(e) {
+    editor.value += e, updateTranscript(editor.value)
+    editor.focus()
+}
+
+function updateTranscript(e) {
+    let t = e.split("\n"),
+        r = "";
+    for (let a = 0; a < t.length; a++) {
+        let i = t[a].trim();
+        if (i.startsWith("I:") && /\[\d{1,2}:\d{2}\]/.test(i)) {
+          // allow <b> tags for bold formatting
+          r += "<b>" + i + "</b><br><br>";
+        } else if (i.startsWith("I:")) {
+          // strip any other HTML tags and allow only line breaks
+          r += i.replace(/(<([^>]+)>)/gi, "") + "<br><br>";
+        } else if (i.startsWith("P:")) {
+          // strip any other HTML tags and allow only line breaks
+          r += i.replace(/(<([^>]+)>)/gi, "") + "<br><br>";
+        } else {
+          // strip any other HTML tags and allow only line breaks
+          r += i.replace(/(<([^>]+)>)/gi, "");
+        }
+    }
+    transcript.innerHTML = r;
+  }
+
+function addBrackets() {
+    let e = document.getElementById("editor"),
+        t = e.selectionStart,
+        r = e.selectionEnd,
+        a = e.value;
+    e.value = a.substring(0, t) + "[]" + a.substring(r), e.selectionStart = t + 1, e.selectionEnd = t + 1, e.focus()
+}
+
+
+editor.addEventListener("keydown", e => {
+    if ("?" === e.key) {
+        editor.value.trim().split("\n").pop(), editor.value += prefix + " []\n", editor.focus();
+        let t = editor.value.length - 2;
+        editor.selectionStart = t, editor.selectionEnd = t, updateTranscript(editor.value), e.preventDefault()
+    }
+}), editor.addEventListener("input", e => {
+    updateTranscript(editor.value)
+}), updateTranscript(editor.value);
+const audioPlayer = document.getElementById("audioPlayer"),
+    playBtn = document.getElementById("playBtn"),
+    pauseBtn = document.getElementById("pauseBtn"),
+    speedSelect = document.getElementById("speedSelect"),
+    audioFile = document.getElementById("audioFile"),
+    frwdBtn = document.getElementById("frwd5"),
+    bckwdBtn = document.getElementById("bckwd5");
+playBtn.addEventListener("click", () => {
+    audioPlayer.play()
+}), pauseBtn.addEventListener("click", () => {
+    audioPlayer.pause()
+}), frwdBtn.addEventListener("click", () => {
+    audioPlayer.currentTime += 5
+}), bckwdBtn.addEventListener("click", () => {
+    audioPlayer.currentTime -= 5
+}), speedSelect.addEventListener("change", () => {
+    audioPlayer.playbackRate = speedSelect.value
+}), audioFile.addEventListener("change", () => {
+    let e = audioFile.files[0],
+        t = new FileReader;
+    t.addEventListener("load", () => {
+        audioPlayer.src = t.result
+    }), t.readAsDataURL(e)
+});
+const getTime = document.getElementById("getTime");
+
+function formatTime(e) {
+    let t = String(Math.floor(e / 60)).padStart(2, "0"),
+        r = String(Math.floor(e % 60)).padStart(2, "0");
+    return `${t}:${r}`
+}
+getTime.addEventListener("click", () => {
+    let start = editor.selectionStart
+    let end = editor.selectionEnd
+    let textTwo = editor.value.substring(editor.selectionEnd);
+    let textOne = editor.value.substring(editor.selectionEnd, 0);
+    editor.value = textOne + "[" + formatTime(audioPlayer.currentTime) + "]" + textTwo, editor.setSelectionRange(end + 7, end + 7), updateTranscript(editor.value), editor.focus()
+}), localStorage.getItem("editorValue") && (editor.value = localStorage.getItem("editorValue"), updateTranscript(editor.value)), editor.addEventListener("input", e => {
+    localStorage.setItem("editorValue", editor.value), updateTranscript(editor.value)
+});
+
